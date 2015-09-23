30edf3767341b5f25bab60635c89265221cbf6f0
lishengzxc <eric@lishengcn.cn>
2015-09-24 00:43:45 +0800

    构建目录

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel'
+    }, {
+      test: /\.scss$/,
+      loader: 'style!css?modules!autoprefixer-loader!sass'
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: 'url-loader?size=8192'
+    }]
+  }
+};
